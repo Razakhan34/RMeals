b3a9444a9bec7829616c8ed22dc54e15b0151c6b
raza-coder8693 <mohdraza8693.mr@gmail.com>
2025-01-13 13:28:46 +0530

    Worked done on AddressAutoFill when user add the address to receive the order

--- React Frontend/src/customers/pages/Cart/Cart.jsx
+++ React Frontend/src/customers/pages/Cart/Cart.jsx
@@ -1,65 +1,58 @@
-import { Button, Card, Divider, Snackbar } from "@mui/material";
-import React, { Fragment, useEffect, useState } from "react";
-import AddressCard from "../../components/Address/AddressCard";
-import CartItemCard from "../../components/CartItem/CartItemCard";
+import React, { useState, Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import {
+  Button,
+  Card,
+  Divider,
+  Snackbar,
+  Box,
+  Grid,
+  TextField,
+} from "@mui/material";
 import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
-import { Box, Modal, Grid, TextField } from "@mui/material";
+import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { AddressAutofill } from "@mapbox/search-js-react";
 import { createOrder } from "../../../State/Customers/Orders/Action";
 import { findCart } from "../../../State/Customers/Cart/cart.action";
 import { isValid } from "../../util/ValidToOrder";
 import { cartTotal } from "./totalPay";
-import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
+import AddressCard from "../../components/Address/AddressCard";
+import CartItemCard from "../../components/CartItem/CartItemCard";
+// import "mapbox-gl/dist/mapbox-gl.css";
 
+// Initial Values and Validation Schema
 const initialValues = {
-  streetAddress: "",
+  address_line1: "",
   state: "",
   pincode: "",
   city: "",
 };
 
-const validationSchema = Yup.object().shape({
-  streetAddress: Yup.string().required("Street Address is required"),
-  state: Yup.string().required("State is required"),
-  pincode: Yup.string()
-    .required("Pincode is required")
-    .matches(/^\d{6}$/, "Pincode must be 6 digits"),
-  city: Yup.string().required("City is required"),
-});
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  outline: "none",
-  p: 4,
-};
+// const validationSchema = Yup.object().shape({
+//   address-line1: Yup.string().required("Street Address is required"),
+//   state: Yup.string().required("State is required"),
+//   pincode: Yup.string()
+//     .required("Pincode is required")
+//     .matches(/^\d{6}$/, "Pincode must be 6 digits"),
+//   city: Yup.string().required("City is required"),
+// });
 
 const Cart = () => {
-  const [openSnackbar, setOpenSnakbar] = useState();
+  const [showAddressForm, setShowAddressForm] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
   const dispatch = useDispatch();
   const { cart, auth } = useSelector((store) => store);
-  const [openAddressModal, setOpenAddressModal] = useState(false);
-  console.log("cart ", cart);
 
-  const handleCloseAddressModal = () => {
-    setOpenAddressModal(false);
-  };
-
-  const handleOpenAddressModal = () => setOpenAddressModal(true);
-
-  console.log(auth.user);
+  const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
   useEffect(() => {
     dispatch(findCart(localStorage.getItem("jwt")));
   }, [dispatch]);
+
+  const handleShowAddressForm = () => setShowAddressForm(!showAddressForm);
+  const handleCloseSnackbar = () => setOpenSnackbar(false);
 
   const handleSubmit = (values, { resetForm }) => {
     const data = {
@@ -68,7 +61,7 @@ const Cart = () => {
         restaurantId: cart.cartItems[0].food?.restaurant.id,
         deliveryAddress: {
           fullName: auth.user?.fullName,
-          streetAddress: values.streetAddress,
+          streetAddress: values["address-line1 address-search"],
           city: values.city,
           state: values.state,
           postalCode: values.pincode,
@@ -76,10 +69,12 @@ const Cart = () => {
         },
       },
     };
-    console.log("data", data);
+
     if (isValid(cart.cartItems)) {
       dispatch(createOrder(data));
-    } else setOpenSnakbar(true);
+    } else {
+      setOpenSnackbar(true);
+    }
   };
 
   const createOrderUsingSelectedAddress = (deliveryAddress) => {
@@ -87,30 +82,24 @@ const Cart = () => {
       token: localStorage.getItem("jwt"),
       order: {
         restaurantId: cart.cartItems[0].food.restaurant.id,
-        deliveryAddress: {
-          fullName: deliveryAddress.fullName,
-          streetAddress: deliveryAddress.streetAddress,
-          city: deliveryAddress.city,
-          state: deliveryAddress.state,
-          postalCode: deliveryAddress.postalCode,
-          country: deliveryAddress.country,
-        },
+        deliveryAddress,
       },
     };
 
     if (isValid(cart.cartItems)) {
       dispatch(createOrder(data));
-    } else setOpenSnakbar(true);
+    } else {
+      setOpenSnackbar(true);
+    }
   };
-
-  const handleCloseSankBar = () => setOpenSnakbar(false);
 
   return (
     <Fragment>
       {cart.cartItems.length > 0 ? (
         <main className="lg:flex justify-between">
+          {/* Left Section - Cart Items */}
           <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10">
-            {cart.cartItems.map((item, i) => (
+            {cart.cartItems.map((item) => (
               <CartItemCard item={item} key={item.id} />
             ))}
 
@@ -123,11 +112,11 @@ const Cart = () => {
                   <p>₹{cartTotal(cart.cartItems)}</p>
                 </div>
                 <div className="flex justify-between text-gray-400">
-                  <p>Deliver Fee</p>
+                  <p>Delivery Fee</p>
                   <p>₹21</p>
                 </div>
                 <div className="flex justify-between text-gray-400">
-                  <p>Plateform Fee</p>
+                  <p>Platform Fee</p>
                   <p>₹5</p>
                 </div>
                 <div className="flex justify-between text-gray-400">
@@ -142,14 +131,17 @@ const Cart = () => {
               </div>
             </div>
           </section>
+
           <Divider orientation="vertical" flexItem />
+
+          {/* Right Section - Address Selection */}
           <section className="lg:w-[70%] flex justify-center px-5 pb-10 lg:pb-0">
-            <div className="">
+            <div>
               <h1 className="text-center font-semibold text-2xl py-10">
                 Choose Delivery Address
               </h1>
               <div className="flex gap-5 flex-wrap justify-center">
-                {auth.user?.addresses.map((item, index) => (
+                {auth.user?.addresses.map((item) => (
                   <AddressCard
                     handleSelectAddress={createOrderUsingSelectedAddress}
                     item={item}
@@ -158,13 +150,13 @@ const Cart = () => {
                   />
                 ))}
 
-                <Card className="flex flex-col justify-center items-center p-5  w-64 ">
+                <Card className="flex flex-col justify-center items-center p-5 w-64">
                   <div className="flex space-x-5">
                     <AddLocationAltIcon />
                     <div className="space-y-5">
                       <p>Add New Address</p>
                       <Button
-                        onClick={handleOpenAddressModal}
+                        onClick={handleShowAddressForm}
                         sx={{ padding: ".75rem" }}
                         fullWidth
                         variant="contained"
@@ -174,42 +166,135 @@ const Cart = () => {
                     </div>
                   </div>
                 </Card>
+
+                {/* Address Form */}
+                {showAddressForm && (
+                  <Box sx={{ marginTop: 2, width: "50%" }}>
+                    <Formik
+                      initialValues={initialValues}
+                      // validationSchema={validationSchema}
+                      onSubmit={handleSubmit}
+                    >
+                      <AddressAutofill accessToken={accessToken}>
+                        <Form>
+                          <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                              <Field
+                                name="address-line1"
+                                as={TextField}
+                                label="Street Address"
+                                fullWidth
+                                variant="outlined"
+                                autoComplete="address-line1"
+                                error={false}
+                                helperText={
+                                  <ErrorMessage name="address-line1">
+                                    {(msg) => (
+                                      <span className="text-red-600">
+                                        {msg}
+                                      </span>
+                                    )}
+                                  </ErrorMessage>
+                                }
+                                sx={{
+                                  "& .MuiOutlinedInput-root": {
+                                    padding: "12px", // Increase padding for all fields
+                                  },
+                                }}
+                              />
+                            </Grid>
+                            <Grid item xs={6}>
+                              <Field
+                                name="state"
+                                as={TextField}
+                                label="State"
+                                fullWidth
+                                variant="outlined"
+                                autoComplete="address-level1"
+                                error={false}
+                                helperText={
+                                  <ErrorMessage name="state">
+                                    {(msg) => (
+                                      <span className="text-red-600">
+                                        {msg}
+                                      </span>
+                                    )}
+                                  </ErrorMessage>
+                                }
+                                sx={{
+                                  "& .MuiOutlinedInput-root": {
+                                    padding: "12px", // Increase padding for all fields
+                                  },
+                                }}
+                              />
+                            </Grid>
+                            <Grid item xs={6}>
+                              <Field
+                                name="pincode"
+                                as={TextField}
+                                label="Pincode"
+                                fullWidth
+                                variant="outlined"
+                                autoComplete="postal-code"
+                                error={false}
+                                helperText={
+                                  <ErrorMessage name="pincode">
+                                    {(msg) => (
+                                      <span className="text-red-600">
+                                        {msg}
+                                      </span>
+                                    )}
+                                  </ErrorMessage>
+                                }
+                                sx={{
+                                  "& .MuiOutlinedInput-root": {
+                                    padding: "12px", // Increase padding for all fields
+                                  },
+                                }}
+                              />
+                            </Grid>
+                            <Grid item xs={12}>
+                              <Field
+                                name="city"
+                                as={TextField}
+                                label="City"
+                                fullWidth
+                                variant="outlined"
+                                autoComplete="address-level2"
+                                error={false}
+                                helperText={
+                                  <ErrorMessage name="city">
+                                    {(msg) => (
+                                      <span className="text-red-600">
+                                        {msg}
+                                      </span>
+                                    )}
+                                  </ErrorMessage>
+                                }
+                                sx={{
+                                  "& .MuiOutlinedInput-root": {
+                                    padding: "12px", // Increase padding for all fields
+                                  },
+                                }}
+                              />
+                            </Grid>
+                            <Grid item xs={12}>
+                              <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                              >
+                                Deliver Here
+                              </Button>
+                            </Grid>
+                          </Grid>
+                        </Form>
+                      </AddressAutofill>
+                    </Formik>
+                  </Box>
+                )}
               </div>
             </div>
-            {/* <div className="flex justify-center items-center h-[90vh]">
-              <Card className="billDetails px-5 text-sm w-[20vw] p-10 space-y-5">
-                <p className=" text-xl font-bold text-center">Bill Details</p>
-                <div className="space-y-3">
-                  <div className="flex justify-between text-gray-400">
-                    <p>Item Total</p>
-                    <p>₹{cartTotal(cart.cartItems)}</p>
-                  </div>
-                  <div className="flex justify-between text-gray-400">
-                <p>Deliver Fee</p>
-                <p>₹21</p>
-              </div> 
-                  <div className="flex justify-between text-gray-400">
-                <p>Plateform Fee</p>
-                <p>₹5</p>
-              </div> 
-                  <div className="flex justify-between text-gray-400">
-                <p>GST and Restaurant Charges</p>
-                <p>₹33</p>
-              </div>
-                  <Divider />
-                  <div className="flex justify-between text-gray-400">
-                    <p>Total Pay</p>
-                    <p>₹{cartTotal(cart.cartItems)}</p>
-                  </div>
-                </div>
-                <Button
-                  onClick={createOrderUsingSelectedAddress}
-                  variant="contained"
-                >
-                  Checkout
-                </Button>
-              </Card>
-            </div> */}
           </section>
         </main>
       ) : (
@@ -220,91 +305,14 @@ const Cart = () => {
           </div>
         </div>
       )}
-      <Modal open={openAddressModal} onClose={handleCloseAddressModal}>
-        <Box sx={style}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            <Form>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Field
-                    name="streetAddress"
-                    as={TextField}
-                    label="Street Address"
-                    fullWidth
-                    variant="outlined"
-                    error={!ErrorMessage("streetAddress")}
-                    helperText={
-                      <ErrorMessage name="streetAddress">
-                        {(msg) => <span className="text-red-600">{msg}</span>}
-                      </ErrorMessage>
-                    }
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <Field
-                    name="state"
-                    as={TextField}
-                    label="State"
-                    fullWidth
-                    variant="outlined"
-                    error={!ErrorMessage("state")}
-                    helperText={
-                      <ErrorMessage name="state">
-                        {(msg) => <span className="text-red-600">{msg}</span>}
-                      </ErrorMessage>
-                    }
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <Field
-                    name="pincode"
-                    as={TextField}
-                    label="Pincode"
-                    fullWidth
-                    variant="outlined"
-                    error={!ErrorMessage("pincode")}
-                    helperText={
-                      <ErrorMessage name="pincode">
-                        {(msg) => <span className="text-red-600">{msg}</span>}
-                      </ErrorMessage>
-                    }
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    name="city"
-                    as={TextField}
-                    label="City"
-                    fullWidth
-                    variant="outlined"
-                    error={!ErrorMessage("city")}
-                    helperText={
-                      <ErrorMessage name="city">
-                        {(msg) => <span className="text-red-600">{msg}</span>}
-                      </ErrorMessage>
-                    }
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button type="submit" variant="contained" color="primary">
-                    Deliver Here
-                  </Button>
-                </Grid>
-              </Grid>
-            </Form>
-          </Formik>
-        </Box>
-      </Modal>
+
+      {/* Snackbar */}
       <Snackbar
         severity="success"
         open={openSnackbar}
         autoHideDuration={6000}
-        onClose={handleCloseSankBar}
-        message="Please Add Items Only From One Restaurants At time"
+        onClose={handleCloseSnackbar}
+        message="Please Add Items Only From One Restaurant At a Time"
       />
     </Fragment>
   );
