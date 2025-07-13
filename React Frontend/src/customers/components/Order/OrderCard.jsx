210695878b90fc4e3cfa3ed46710aa432a83e3a9
Razakhan34 <mohdraza121.mr@gmail.com>
2025-07-13 17:26:28 +0530

    Fixes Some bugs in the application

--- React Frontend/src/customers/components/Order/OrderCard.jsx
+++ React Frontend/src/customers/components/Order/OrderCard.jsx
@@ -13,32 +13,36 @@ const OrderCard = ({ order, status, orderId }) => {
           <p className="text-gray-400">₹{order.food.price}</p>
         </div>
       </div>
-      <div>
-        <Chip
-          sx={{
-            color: "white !important",
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
-          label={status}
-          size="small"
-          color={
-            status === "PENDING"
-              ? "info"
-              : status === "DELIVERED"
-              ? "success"
-              : "secondary"
-          }
-          className="text-white"
-        />
+
+      <div className="flex items-center">
+        <div className="mr-10">
+          <Chip
+            sx={{
+              color: "white !important",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+            label={status}
+            size="small"
+            color={
+              status === "PENDING"
+                ? "info"
+                : status === "DELIVERED"
+                ? "success"
+                : "secondary"
+            }
+            className="text-white"
+          />
+        </div>
+        {/* Styled Track Order Link */}
+
+        <Link
+          to={`/track-order/${orderId}`}
+          className="text-blue-500 font-semibold underline hover:text-blue-700 transition duration-300"
+        >
+          Track order
+        </Link>
       </div>
-      {/* Styled Track Order Link */}
-      <Link
-        to={`/track-order/${orderId}`}
-        className="text-blue-500 font-semibold underline hover:text-blue-700 transition duration-300"
-      >
-        Track order
-      </Link>
     </Card>
   );
 };
