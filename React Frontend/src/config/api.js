8d556371af266d0d57fd01b72a1030ee3892109f
Razakhan34 <mohdraza121.mr@gmail.com>
2025-09-21 12:56:39 +0530

    change backend hostep url in frontend react and also create Dockerfile in backend

--- React Frontend/src/config/api.js
+++ React Frontend/src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:8081";
+// export const API_URL = "http://localhost:8081";
+export const API_URL = "https://rmeals-backend.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
